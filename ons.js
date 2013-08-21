5e298d484127fcd376d70fb781110ce8c291df7a
XiNGRZ <chenxingyu92@gmail.com>
2013-08-21 15:07:03 +0800

    cases of different answer

--- ons.js
+++ ons.js
@@ -1,4 +1,10 @@
-(function () {
+(function (ONS) {
+    if ('undefined' == typeof exports) {
+        window.ONS = ONS;
+    } else {
+        module.exports = ONS;
+    }
+})(function () {
 
     'use strict';
 
@@ -6,19 +12,34 @@
         this.night = 1;
     };
 
-    ONS.prototype.starter = function () {
+    ONS.prototype.starter = function (prompt) {
+        var self = this;
+        
         // 搭讪开场白
-        window.prompt("So... What's your favorite programming language?");
+        prompt("So... What's your favorite programming language?", function (answer) {
+            switch (answer) {
+                case "JavaScript":
+                    self.make();
+                    break;
+                case "CoffeeScript":
+                    // ignore
+                    break;
+                case "PHP":
+                    throw new Error("Sounds great!");
+                default:
+                    try {
+                        self.make();
+                    } catch (e) {
+                        throw new Error("I like it too.");
+                    }
+            }
+        });
     };
 
     ONS.prototype.make = function () {
         // 做爱做的事
     };
 
-    if ('undefined' == typeof exports) {
-        window.ONS = ONS;
-    } else {
-        module.exports = ONS;
-    }
+    return ONS;
 
-})();
+});
